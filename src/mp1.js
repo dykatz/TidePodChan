ded808a04b0e5d32bc9c91d94802bbcc8ddc22db
Dylan Katz <dykatz@uw.edu>
2018-01-09 11:09:43 -0800

    Add cursor

--- src/mp1.js
+++ src/mp1.js
@@ -2,23 +2,15 @@ class MP1 extends Game {
 	constructor(arg) {
 		super(arg, 0.9, 0.9, 0.9);
 		this.colorShader = new SimpleShader(this);
-		this.camera = new Camera(this, vec2.fromValues(20, 60), 20,
-			[20, 40, 600, 300]);
+		this.camera = new Camera(this, vec2.fromValues(0, 0), 200,
+			[5, 5, 630, 630]);
 
-		this.white_sq = new Renderable(this, this.colorShader);
-		this.white_sq.color = [1.0, 1.0, 1.0, 1.0];
-		this.white_sq.xform.x = 20;
-		this.white_sq.xform.y = 60;
-		this.white_sq.xform.width = 5;
-		this.white_sq.xform.height = 5;
-		this.white_sq.xform.rot_rad = 0.2;
-
-		this.red_sq = new Renderable(this, this.colorShader);
-		this.red_sq.color = [1.0, 0.0, 0.0, 1.0];
-		this.red_sq.xform.x = 20;
-		this.red_sq.xform.y = 60;
-		this.red_sq.xform.width = 2;
-		this.red_sq.xform.height = 2;
+		this.cursor = new Renderable(this, this.colorShader);
+		this.cursor.color = [1.0, 0.0, 0.0, 1.0];
+		this.cursor.xform.x = 0;
+		this.cursor.xform.y = 0;
+		this.cursor.xform.width = 1;
+		this.cursor.xform.height = 1;
 
 		this.delete_mode = false;
 	}
@@ -27,33 +19,24 @@ class MP1 extends Game {
 		if (this.isKeyReleased(Key.D))
 			this.delete_mode = !this.delete_mode;
 
+		if (this.isKeyDown(Key.A) || this.isKeyDown(Key.Left))
+			this.cursor.xform.x -= dt * 50;
+		if (this.isKeyDown(Key.D) || this.isKeyDown(Key.Right))
+			this.cursor.xform.x += dt * 50;
+		if (this.isKeyDown(Key.W) || this.isKeyDown(Key.Up))
+			this.cursor.xform.y += dt * 50;
+		if (this.isKeyDown(Key.S) || this.isKeyDown(Key.Down))
+			this.cursor.xform.y -= dt * 50;
+
+		if (this.cursor.xform.x < -100) this.cursor.xform.x = -100;
+		if (this.cursor.xform.x >  100) this.cursor.xform.x =  100;
+		if (this.cursor.xform.y < -100) this.cursor.xform.y = -100;
+		if (this.cursor.xform.y >  100) this.cursor.xform.y =  100;
+
 		document.getElementById("elapsed").innerHTML = (dt*1000).toFixed(2);
 		document.getElementById("fps").innerHTML = (1/dt).toFixed(2);
 		document.getElementById("objs").innerHTML = 2;
 		document.getElementById("delete-mode").innerHTML = this.delete_mode;
-
-		var wxform = this.white_sq.xform;
-		var rxform = this.red_sq.xform;
-
-		if (this.isKeyDown(Key.Right)) {
-			if (wxform.x > 30)
-				wxform.x = 10;
-
-			wxform.x += 10 * dt;
-		}
-
-		if (this.isKeyPressed(Key.Up))
-			wxform.rot_deg += 1;
-
-		if (this.isKeyDown(Key.Down)) {
-			if (rxform.width > 5) {
-				rxform.width = 2;
-				rxform.height = 2;
-			}
-
-			rxform.width += 10 * dt;
-			rxform.height += 10 * dt;
-		}
 	}
 
 	draw(update_count, lag_time) {
@@ -61,7 +44,6 @@ class MP1 extends Game {
 		document.getElementById("updates").innerHTML = update_count;
 
 		this.camera.setup_vp();
-		this.white_sq.draw(this.camera.vp);
-		this.red_sq.draw(this.camera.vp);
+		this.cursor.draw(this.camera.vp);
 	}
 }
