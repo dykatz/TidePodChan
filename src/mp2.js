d459603a77e0c74516b69e4d0fdb798a380e2b5e
Dylan Katz <dykatz@uw.edu>
2018-01-15 17:07:59 -0800

    wire cam2

--- src/mp2.js
+++ src/mp2.js
@@ -1,11 +1,12 @@
 class Scene1 extends Scene {
-	constructor(game, sq_shader) {
+	constructor(game, sq_shader, cam2) {
 		super(game);
 		this.really_loaded = false;
 		this.sq_shader = sq_shader;
 		this.camera = null;
+		this.camera2 = cam2;
 		this.sqaures = null;
-		this.scene2 = new Scene2(game, sq_shader);
+		this.scene2 = new Scene2(game, sq_shader, cam2);
 	}
 
 	onLoad() {
@@ -37,15 +38,22 @@ class Scene1 extends Scene {
 		this.squares.forEach(s => {
 			s.draw(this.camera.vp);
 		});
+
+		this.camera2.setup_vp();
+
+		this.squares.forEach(s => {
+			s.draw(this.camera2.vp);
+		});
 	}
 }
 
 class Scene2 extends Scene {
-	constructor(game, sq_shader) {
+	constructor(game, sq_shader, cam2) {
 		super(game);
 		this.really_loaded = false;
 		this.sq_shader = sq_shader;
 		this.camera = null;
+		this.camera2 = cam2;
 		this.squares = null;
 	}
 
@@ -72,6 +80,12 @@ class Scene2 extends Scene {
 		this.squares.forEach(s => {
 			s.draw(this.camera.vp);
 		});
+
+		this.camera2.setup_vp();
+
+		this.squares.forEach(s => {
+			s.draw(this.camera2.vp);
+		});
 	}
 }
 
@@ -79,6 +93,23 @@ class MP2 extends Game {
 	constructor(canvas_id) {
 		super(canvas_id, 0.9, 0.9, 0.9);
 		var sq_shader = new SimpleShader(this);
-		this.currentScene = new Scene1(this, sq_shader);
+		this.cam2 = new Camera(this, vec2.fromValues(20, 60), 20,
+			[100, 20, 50, 50]);
+		this.cam2.bg = [0.0, 0.9, 0.9, 1.0];
+		this.currentScene = new Scene1(this, sq_shader, this.cam2);
+	}
+
+	update(dt) {
+		if (this.isKeyReleased(Key.A))
+			this.cam2.viewport[0] -= 10;
+
+		if (this.isKeyReleased(Key.D))
+			this.cam2.viewport[0] += 10;
+
+		if (this.isKeyReleased(Key.S))
+			this.cam2.viewport[1] -= 10;
+
+		if (this.isKeyReleased(Key.W))
+			this.cam2.viewport[1] += 10;
 	}
 }
