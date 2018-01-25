44e04bc38cbfe06e8630674f3694e45e57400f28
Dylan Katz <dykatz@uw.edu>
2018-01-25 13:38:48 -0800

    Load bg image

--- src/mp3.js
+++ src/mp3.js
@@ -1,7 +1,7 @@
 class MP3 extends Game {
 	constructor(canvas_id) {
 		super(canvas_id, 0.9, 0.9, 0.9);
-		this.shader = new TextureShader(this);
+		this.tshader = new TextureShader(this);
 		this.main_camera = new Camera(this, vec2.fromValues(0, 0), 100, [260, 5, 535, 590]);
 		this.anim_camera = new Camera(this, vec2.fromValues(0, 0), 100, [5, 345, 250, 250]);
 		this.anim_camera.bg = [0.8, 1.0, 0.8, 1.0];
@@ -12,11 +12,16 @@ class MP3 extends Game {
 		this.zib2_camera = new Camera(this, vec2.fromValues(0, 0), 100, [132.5, 10+d, d, d]);
 		this.zib3_camera = new Camera(this, vec2.fromValues(0, 0), 100, [130-d/2, 5, d, d]);
 
-		this.frames = new Set();
+		this.fetchImageResource("assets/mp3/minion_sprite.png", n => {
+			this.background = new TextureRenderable(this.tshader, this.getResource(n));
+		});
 	}
 
 	draw(updates, lag_time) {
+		if (!this.background) return;
+
 		this.main_camera.setup_vp();
+		this.background.draw(this.main_camera.vp);
 		this.anim_camera.setup_vp();
 		this.zib0_camera.setup_vp();
 		this.zib1_camera.setup_vp();
